883a3b5848cebf76782bbd91a87dafb3a80de265
tmduddy <tmd44@pitt.edu>
2019-12-04 22:35:01 -0500

    correct naming for day1 (it was pt. 2 not day2) and add comments

--- day1.py
+++ day1.py
@@ -1,9 +1,25 @@
 import csv
 
-with open('input-files/day1.csv', 'r') as input_file:
-    reader = csv.reader(input_file, delimiter=',')
+def rocket_equation(mass):
+    fuel = (mass // 3) - 2
+    return fuel if fuel > 0 else 0
+
+# convert new line delimited file into list of ints
+with open('input-files/day2.csv', 'r') as input_file:
+    reader = csv.reader(input_file)
     input_data = [int(row[0]) for row in reader]
 
-output_fuel = (list(map(lambda x: (x//3) - 2, input_data)))
+final_mass = []
 
-print(sum(output_fuel))
+for mass in input_data:
+    # apply formula to module mass to get initial fuel
+    new_mass = rocket_equation(mass)
+    fuel_mass = new_mass
+
+    # repeat rocket_equation on the fuel mass until 0 is returned. Add the results to the final list
+    while new_mass > 0:
+        new_mass = rocket_equation(new_mass)
+        fuel_mass += new_mass
+    final_mass.append(fuel_mass)
+
+print(sum(final_mass))
