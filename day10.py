5bf24c1af810457140ac3c7d635b515dfa8573a4
tmduddy <tmd44@pitt.edu>
2019-12-15 20:44:15 -0500

    work on 10a

--- day10.py
+++ day10.py
@@ -1,2 +1,10 @@
 with open('input-files/day10.csv', 'r') as f:
-    input_data = [row for row in f]
+    grid = [list(row.strip()) for row in f]
+
+for row in range(len(grid)):
+    for col in range(len(grid[row])):
+        point = grid[row][col]
+        if point == '.':
+            continue
+        
+
